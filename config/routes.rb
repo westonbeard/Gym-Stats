f522f6188cdd6aca472ba30875dc0bbbb7baf0db
weston <weston.beard@gmail.com>
2015-02-23 19:04:00 -0500

    adding login with twitter link to users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'users#index'
   
-  get '/auth/:twitter/callback', to: 'sessions#create'
+  get '/auth/twitter/callback', to: 'sessions#create'
+  get '/auth/failure', to: redirect('/')
+  get '/signout' => 'sessions#destroy', :as => :signout
   
   resources :gyms
 
